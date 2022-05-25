5ac4cea1b9c68197382581ea7adc7c3a8ad1b477
Akshat Adsule <akshat.adsule@gmail.com>
2022-05-24 18:52:50 -0700

    fix ios not getting orientation data

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,10 @@ import {
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const sessionId = urlParams.get("session");
+const permsBtn: HTMLButtonElement = document.getElementById(
+  "perms"
+) as HTMLButtonElement;
+permsBtn.addEventListener("click", getPerms);
 
 async function checkIfDocumentExists(ref: DatabaseReference): Promise<boolean> {
   let data = await get(ref);
@@ -29,20 +33,14 @@ if (sessionId) {
     appId: "1:1034280146144:web:ef8d739c3fd60476132c85",
   };
 
-  const app = initializeApp(firebaseConfig);
-  const rtdb = getDatabase(app);
+  var app = initializeApp(firebaseConfig);
+  var rtdb = getDatabase(app);
 
-  const session = ref(rtdb, `sessions/${urlParams.get("session")}`);
+  var session = ref(rtdb, `sessions/${urlParams.get("session")}`);
 
-  let alpha_p = document.getElementById("alpha")!;
-  let beta_p = document.getElementById("beta")!;
-  let gamma_p = document.getElementById("gamma")!;
-
-  // (async () => {
-  //   await update(session, {
-  //     isClientConnected: true,
-  //   });
-  // })();
+  var alpha_p = document.getElementById("alpha")!;
+  var beta_p = document.getElementById("beta")!;
+  var gamma_p = document.getElementById("gamma")!;
 
   window.addEventListener(
     "deviceorientation",
@@ -69,4 +67,44 @@ if (sessionId) {
   );
 } else {
   document.getElementById("log")!.innerText = "Session ID not provided!";
+}
+
+function getPerms() {
+  console.log("hello");
+  // feature detect
+  if (typeof (DeviceOrientationEvent as any).requestPermission === "function") {
+    (DeviceOrientationEvent as any)
+      .requestPermission()
+      .then((permissionState: any) => {
+        if (permissionState === "granted") {
+          console.log("registered ios orientation listener")
+          window.addEventListener(
+            "deviceorientation",
+            async (event: DeviceOrientationEvent) => {
+              if (await checkIfDocumentExists(session)) {
+                let alpha = Math.round(event.alpha!);
+                let beta = Math.round(event.beta!);
+                let gamma = Math.round(event.gamma!);
+                alpha_p.innerText = `alpha: ${alpha}`;
+                beta_p.innerText = `beta: ${beta}`;
+                gamma_p.innerText = `gamma: ${gamma}`;
+                await update(session, {
+                  orientation: beta,
+                  isClientConnected: true,
+                });
+              } else {
+                alpha_p.innerText = "";
+                beta_p.innerText = "";
+                gamma_p.innerText = "";
+                document.getElementById("log")!.innerText = "Session ended";
+              }
+            },
+            true
+          );
+        }
+      })
+      .catch(console.error);
+  } else {
+    // handle regular non iOS 13+ devices
+  }
 }
